e18e6f5357c39f280ab54ee32a357c3b8d2186ce
Hilary <hilary.koerner@gmail.com>
2021-04-03 16:34:49 -0500

    use index to find first value

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,29 @@
-d3.json("data/samples.json").then((sampleData)=> {
-    console.log(sampleData);
-    var data = sampleData;
+d3.json("data/samples.json").then((data)=> {
+    var samples = data.samples;
+    console.log(samples);
+    var names = data.names;
+    console.log(names);
+    var meta = data.metadata;
+    console.log(meta);
+    var sampleUTO = samples[0];
+    console.log(sampleUTO)
 });
+// first bar chart set up
+// var trace1 = {
+//     x: otu_ids,
+//     y: sample_values,
+//     type: "bar",
+//     orientation: "h",
+// };
+
+// var data = [trace1];
+
+// var layout = {
+//     title: "enter bar title here"
+// };
+// Plotly.newPlot("bar", data, layout);
+
+
 
 
 
