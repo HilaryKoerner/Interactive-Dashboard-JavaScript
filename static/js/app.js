fe326b9d900242e32caed2e9e35b10595c6bec39
Hilary <hilary.koerner@gmail.com>
2021-04-05 08:28:33 -0500

    plotly table

--- static/js/app.js
+++ static/js/app.js
@@ -75,102 +75,31 @@ function createBarChart(data) {
 
 function createBubble(data, names) {
     console.log("hello from create bubble");
-    console.log(data);
+    console.log(data.otu_ids);
+    console.log(data.sample_values);
+    console.log(data.otu_labels);
+    
+    var trace1 = {
+        x: data.otu_ids,
+        y: data.sample_values,
+        text: data.sample_values.map(samp_values =>`size:${samp_values}`),
+        mode: 'markers',
+        marker: {
+          color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+          size: data.sample_values
+        }
+    } 
+      var data = [trace1];
+      
+      var layout = {
+        title: 'Bubble Chart Hover Text',
+        showlegend: false,
+        height: 600,
+        width: 600
+      };
+      
+      Plotly.newPlot('bubble', data, layout);
+
 }
-
-
-//
-// d3.json("data/samples.json").then((data)=> {
-
-
-
-
-
-
-// d3.json("data/samples.json").then((data)=> {
-//     var sampleData = data.samples
-//     console.log(sampleData)
-// });
-
-// sampleData.forEach((sample) => {
-//     console.log(sample[1]);
-  
-//     // Get the entries for each object in the array
-//     Object.entries(sample).forEach(([key, value]) => {
-//     //   Log the key and value
-//       console.log(`Key: ${key} and Value ${value}`);
-//     });
-//   });
-  
-  
-// var sampleID = data.map(sample => sample[0]);
-// var sampleOTU = data.map(sample => sample[1]);
-  
-// console.log(sampleID);
-// console.log(sampleOTU);
-
-
-
-// var ids = [];
-// var OTU_ids = [];
-// var OTU_labels =[];
-
-// // Iterate through each recipe object
-// sampleData.forEach((sample) => {
-
-//   // Iterate through each key and value
-//   Object.entries(sample).forEach(([key, value]) => {
-
-//     // Use the key to determine which array to push the value to
-//     if (key === "id") {
-//         ids.push(value);
-//     }
-//     else if (key === "otu_ids" {
-//         OTU_ids.push(value);
-//     }
-//     else {
-//         OTU_labels.push(value);
-//      });
-//   });
-  
-
-
-
-
-
-
-// let trace1 = {
-//     x: [],
-//     y: [],
-//     mode: "bar"
-// };
-// var data = [trace1];
-
-// data.forEach(function(samp) {
-// trace1.x.push(samp["sample_values"]);
-// trace1.y.push(samp["otu_ids"]);
-// });
-// var layout = {
-//     title: "enter bar title here"
-// };
-// Plotly.newPlot('bar', data, layout);
-
-//  bar chart set up
-// var trace1 = {
-//     x: otu_ids,
-//     y: sample_values,
-//     type: "bar",
-//     orientation: "h",
-// };
-
-// var data = [trace1];
-
-// var layout = {
-//     title: "enter bar title here"
-// };
-// Plotly.newPlot("bar", data, layout);
-
-
-
 
 
